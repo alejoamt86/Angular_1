8e7b12cec561caef284db1e168a5a41137c3c762
Alejandro Mejia <alejoamt@gmail.com>
2019-11-20 09:54:11 -0500

    nfor

---
+++ listado-personas/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Persona} from './persona.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'listado de personas';
+
+  personas: Persona[] = [new Persona('Juan','Perez'), new Persona("Laura","lola")];
+  nombreInput:string;
+  apellidoInput:string;
+
+  onAgregarPersona(){
+    let persona1 = new Persona(this.nombreInput,this.apellidoInput);
+    this.personas.push(persona1);
+
+  }
+
+}
