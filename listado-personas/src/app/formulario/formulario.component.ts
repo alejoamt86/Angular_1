4d9e1889b0a7300c897f51f0006f0fb41ae09af3
Alejandro Mejia <alejoamt@gmail.com>
2019-11-21 11:43:36 -0500

    ..

--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -18,7 +18,7 @@ export class FormularioComponent implements OnInit {
 
   onAgregarPersona(){
     let persona1 = new Persona(this.nombreInput,this.apellidoInput);
-    // this.personas.push(persona1);
+    // 
     this.personaCreada.emit(persona1);
   }
 
